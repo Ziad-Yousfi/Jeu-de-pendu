7b4b3b685d70dd5d6189f1d725ed28d658a1470e
Ziad Yousfi <yousfiziadpro@gmail.com>
2025-05-04 13:36:20 +0100

    Add files via upload

---
+++ Jeu_de_Pendu/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(401, 234)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(175, 236)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(146, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Entrer une lettre :"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(641, 231)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 2
+        Button1.Text = "Valider"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(97, 60)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(218, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "Nombre d'essaie restant : "
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(321, 60)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 25)
+        Label3.TabIndex = 4
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(623, 60)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(237, 25)
+        Label4.TabIndex = 5
+        Label4.Text = "Nombre de lettre à devinée :"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(860, 60)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 25)
+        Label5.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1041, 552)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+
+End Class
---
+++ Jeu_de_Pendu/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Dim motSecret As String
+    Dim cpt As Integer = 6
+    Dim nbLettres As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Les régles du jeux : entré un mot de votre choix, et un autre joueur devra le deviner en moins de 6 coups.")
+        motSecret = InputBox("Entrez le mot à rechercher :", "Mot Secret").ToUpper()
+        Label3.Text = cpt.ToString
+        nbLettres = motSecret.Length
+        Label5.Text = nbLettres.ToString
+        If String.IsNullOrEmpty(motSecret) Then
+            MessageBox.Show("Vous devez entrer un mot valide.")
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim lettreProposee As String = TextBox1.Text.ToUpper()
+
+        If motSecret.Contains(lettreProposee) Then
+            MsgBox("Bien joué ! La lettre est dans le mot.")
+            nbLettres = nbLettres - 1
+            Label5.Text = nbLettres.ToString
+        Else
+            MsgBox("Dommage ! La lettre n'est pas dans le mot.")
+            cpt = cpt - 1
+            Label3.Text = cpt.ToString
+        End If
+        If cpt = 0 Then
+            MessageBox.Show("Vous avez perdu ! Le mot était : " & motSecret)
+            Me.Close()
+        End If
+        If nbLettres = 0 Then
+            MessageBox.Show("Vous avez gagné ! Le mot était : " & motSecret)
+            Me.Close()
+        End If
+    End Sub
+End Class
